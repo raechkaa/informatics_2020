84374edee24b711fb081777a742d50d53201c272
raechkaa <71277041+raechkaa@users.noreply.github.com>
2020-10-12 01:09:26 +0300

    Add files via upload

--- 3week/ex2.py
+++ 3week/ex2.py
@@ -1,4 +1,5 @@
+import shutil
 with open("./упр1.txt", "r") as file1, open("./упр2.txt", "w") as file2:
-    file2.write(file1.read())
+     shutil.copyfileobj(file1, file2)
 file1.close()
 file2.close()
