50d8994a2985d48bd015da4310d39019b2ae34b6
raechkaa <71277041+raechkaa@users.noreply.github.com>
2020-12-17 01:08:32 +0300

    Add files via upload

---
+++ 11_week/ex. 2.py
@@ -0,0 +1,33 @@
+import asyncio
+from concurrent.futures import FIRST_COMPLETED
+from collections import namedtuple
+import aiohttp
+
+Service = namedtuple('Service', ('name', 'url', 'ip_attr'))
+
+SERVICES = (
+    Service('ipify', 'https://api.ipify.org?format=json', 'ip'),
+    Service('ip-api', 'http://ip-api.com/json', 'query')
+)
+
+
+async def fetch_ip(session, service):
+    async with session.get(service.url) as response:
+        html = await response.json()
+        return html
+
+
+async def asynchronous():
+    async with aiohttp.ClientSession() as session:
+        fut = await asyncio.wait({fetch_ip(session, serv) for serv in SERVICES}, return_when=FIRST_COMPLETED)
+        for i in fut[0]:
+            a = i.result()
+            for serv in SERVICES:
+                try:
+                    print(a[serv.ip_attr])
+                except:
+                    pass
+
+
+ioloop = asyncio.get_event_loop()
+ioloop.run_until_complete(asynchronous())
