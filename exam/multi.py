690aaa059633bf99af95edc9f854007a84ba6c29
raechkaa <71277041+raechkaa@users.noreply.github.com>
2021-01-09 13:59:15 +0300

    Add files via upload

--- exam/multi.py
+++ exam/multi.py
@@ -10,10 +10,12 @@ def counter(file):
 
 
 if __name__ == '__main__':
-    pool = Pool(processes=4)
+    pool = Pool(processes=5)
+    files_len = 1000
     start = time.time()
-    print(pool.map(counter, ['1.txt', '2.txt', '3.txt']))
+    print(pool.map(counter, ["./{}.txt".format(i) for i in range(files_len)]))
     print(time.time() - start)
 # время = 0.25771117210388184
 # вывод = [40, 42, 46]
 # потому что я создала всего 3 файла и поэтому простая реализация эффективней чем с мультипроцессорами
+# для 1000 файлов время = 0.17364239692687988
